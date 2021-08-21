f3cb90be9bf46704fca20d7101b1c19b1e8b13c3
Fitri Wibowo <parcoxs@gmail.com>
2021-08-21 14:15:06 +0700

    Filter emodul pada list_display berdasarkan user yang login. Kecuali superuser

--- emodul/admin.py
+++ emodul/admin.py
@@ -1,37 +1,42 @@
 from django.contrib import admin
 from .models import EModul, EModulComment, EModulDetail, EModulBookmark, EModulAnnotation
 from akademik.models import MataKuliah
+from accounts.models import CustomUser
+
 
 @admin.register(EModul)
 class EModulAdmin(admin.ModelAdmin):
-    list_display = ['mata_kuliah', 'judul', 'jumlah_modul','tanggal']
+    list_display = ['mata_kuliah', 'judul', 'jumlah_modul', 'tanggal']
 
-    def queryset(self, request):
-        qs = super(admin.ModelAdmin, self).queryset(request)
+    def get_queryset(self, request):
+        qs = super(admin.ModelAdmin, self).get_queryset(request)
 
         if request.user.is_superuser:
             return qs
 
-        user_qs = pengajar.objects.filter(user=request.user)
-        return qs.filter(pengajar=user_qs)
+        logged_in_user = CustomUser.objects.filter(
+            username=request.user.username).get()
+        filter_matakuliah = MataKuliah.objects.filter(pengajar=logged_in_user)
+        emodul_terfilter = EModul.objects.filter(
+            mata_kuliah__in=filter_matakuliah)
+        return emodul_terfilter
 
 
 @admin.register(EModulDetail)
 class EModulDetailAdmin(admin.ModelAdmin):
     list_display = ['emodul', 'judul', 'jumlah_halaman', 'file']
 
+
 @admin.register(EModulAnnotation)
 class EModulAnnotationAdmin(admin.ModelAdmin):
     list_display = ['dokumen', 'user', 'koordinat', 'halaman', 'text']
 
+
 @admin.register(EModulBookmark)
 class EModulBookmarkAdmin(admin.ModelAdmin):
-    list_display = ['dokumen', 'user', 'halaman','tanggal']
+    list_display = ['dokumen', 'user', 'halaman', 'tanggal']
+
 
 @admin.register(EModulComment)
 class EModulCommentAdmin(admin.ModelAdmin):
     list_display = ['dokumen', 'user', 'comment']
-
-
-
-
